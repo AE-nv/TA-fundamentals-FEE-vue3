204f8bd08cc322a108cfe521a849c2456b6689a7
Axel Lemmens <axel.lemmens@ae.be>
2022-09-13 07:17:08 +0200

    Solution to exercise 3

--- src/components/Quest/CarQuest.vue
+++ src/components/Quest/CarQuest.vue
@@ -6,10 +6,23 @@ import TextAreaBox from "../shared/TextAreaBox.vue";
 
 const isQuestComplete = () => false;
 
-const state = reactive<{ foundCars: Car[] }>({ foundCars: [] });
+const state = reactive<{ foundCars: Car[]; recommendations: Car[] }>({
+  foundCars: [],
+  recommendations: [],
+});
 
 const searchForCars = (value: string) => {
   state.foundCars = carSearchService.search(value);
+};
+
+const recommend = (value: Car) => {
+  state.recommendations.push(value);
+  state.foundCars.splice(state.foundCars.indexOf(value), 1);
+};
+
+const unrecommend = (value: Car) => {
+  state.foundCars.push(value);
+  state.recommendations.splice(state.recommendations.indexOf(value), 1);
 };
 </script>
 
@@ -52,6 +65,18 @@ const searchForCars = (value: string) => {
             <h3 class="quest-header">Recommended Cars</h3>
 
             <!-- TODO: Show list of recommended cars -->
+            <div v-for="(car, index) in state.recommendations" :key="index">
+              <v-card class="car-card">
+                <div class="car-card-text">
+                  {{ car.make_display }} - {{ car.model_trim }}
+                </div>
+                <div class="car-card-button">
+                  <v-btn text icon @click="unrecommend(car)">
+                    <v-icon>mdi-thumb-down</v-icon>
+                  </v-btn>
+                </div>
+              </v-card>
+            </div>
           </v-container>
         </v-card>
       </div>
@@ -61,6 +86,11 @@ const searchForCars = (value: string) => {
           <v-container>
             <h3 class="quest-header">Search for Cars</h3>
             <!-- TODO 1: include the text-area-box component -->
+            <!-- TODO 2: implement function that calls the CarSearchService -->
+            <!-- TODO 3: show list of cars returned by the CarSearchService -->
+            <!-- TODO 4: implement function to recommend car -->
+            <!-- TODO 5: implement function to unrecommend car -->
+            <!-- TODO 6: implement a message to show when the list of cars is empty -->
             <TextAreaBox
               @text="searchForCars"
               btnText="Search cars"
@@ -68,14 +98,16 @@ const searchForCars = (value: string) => {
             />
             <div v-for="(car, index) in state.foundCars" :key="index">
               <v-card class="car-card">
-                {{ car.make_display }} - {{ car.model_trim }}
+                <div class="car-card-text">
+                  {{ car.make_display }} - {{ car.model_trim }}
+                </div>
+                <div class="car-card-button">
+                  <v-btn flat icon @click="recommend(car)"
+                    ><v-icon>mdi-thumb-up</v-icon></v-btn
+                  >
+                </div>
               </v-card>
             </div>
-            <!-- TODO 2: implement function that calls the CarSearchService -->
-            <!-- TODO 3: show list of cars returned by the CarSearchService -->
-            <!-- TODO 4: implement function to recommend car -->
-            <!-- TODO 5: implement function to unrecommend car -->
-            <!-- TODO 6: implement a message to show when the list of cars is empty -->
             <!-- TODO 7: extract the list of found and recommended cars to a shared component -->
           </v-container>
         </v-card>
@@ -87,5 +119,24 @@ const searchForCars = (value: string) => {
 <style lang="scss" scoped>
 .v-card {
   margin-bottom: 10px;
+}
+
+.car-card {
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  &-button {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  &-text {
+    display: flex;
+    align-items: center;
+    width: 50%;
+  }
 }
 </style>
